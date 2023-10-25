53f524fdd1d5114b152e9dac1972a1374acaab32
Patrick Lawrence <patrickjameslawrence@icloud.com>
2023-10-25 03:55:10 -0400

    Added account creation

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const { ObjectId } = require('mongodb');
 
 const corsOptions = {
   origin: 'http://localhost:9000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -17,10 +19,6 @@ const { db } = require("./config/db.config");
 
 const posts = db.collection("posts");
 const users = db.collection("users");
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
 
 app.get(process.env.BASE_API_URL + "posts", (req, res) => {
   posts
@@ -41,6 +39,32 @@ app.post(process.env.BASE_API_URL + "posts", async (req, res) => {
   post.timestamp = new Date().getTime();
 
   posts.insertOne(post);
+});
+
+app.post(process.env.BASE_API_URL + "users/sign-up", async (req, res) => {
+  var user = req.body;
+  user.username = user.username.toLowerCase()
+
+  console.log(user);
+  let tempUser = await users.findOne({ email: user.email });
+  if (tempUser) {
+    res.statusCode = 409
+    return res.send("An account is already linked to this email")
+  }
+  tempUser = await users.findOne({username: user.username})
+  if (tempUser) {
+    res.statusCode = 409
+    return res.send("Username has been taken")
+  }
+
+  user = {
+    _id: new ObjectId(),
+    accountCreated: new Date().getTime(),
+    isPrivate: false,
+    ...user
+  };
+
+  users.insertOne(user);
 });
 
 app.listen(process.env.PORT, () => {
